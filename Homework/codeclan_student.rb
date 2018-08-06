3e0359652d1d7228bb29fce584d788cbadc98ee7
Alan Hunter <localpunter@sky.com>
2018-08-06 15:32:06 +0100

    homework part A complete

--- Homework/codeclan_student.rb
+++ Homework/codeclan_student.rb
@@ -1,8 +1,10 @@
 class CodeClanStudent
 
-def initialize(name, cohort)
+def initialize(name, cohort, talk, fav_language)
   @name = name
   @cohort = cohort
+  @talk = talk
+  @fav_language = fav_language
 end
 
 def student_name
@@ -12,6 +14,27 @@ end
 
 def cohort
 return @cohort
+end
+
+def set_student_name(new_name)
+  @name = new_name
+
+end
+
+def set_cohort(new_cohort)
+  @cohort = new_cohort
+
+end
+
+def talk
+  return @talk
+
+
+end
+
+def fav_language
+  return "I love #{@fav_language}"
+
 end
 
 end
--- Homework/specs/codeclan_student_spec.rb
+++ Homework/specs/codeclan_student_spec.rb
@@ -6,7 +6,7 @@ require_relative('../codeclan_student.rb')
 class CodeClanTest < MiniTest::Test
 
 def test_student_name
-  student = CodeClanStudent.new('Gillian', 'E1')
+  student = CodeClanStudent.new('Gillian', 'E1', "I can talk", 'Ruby')
 
   assert_equal('Gillian', student.student_name)
 
@@ -14,10 +14,35 @@ def test_student_name
 end
 
 def test_cohort
-  student = CodeClanStudent.new('Gillian', 'E1')
+  student = CodeClanStudent.new('Gillian', 'E1', "I can talk", 'Ruby')
 
   assert_equal('E1', student.cohort)
 
+end
+
+def test_set_student_name
+  student = CodeClanStudent.new('Gillian', 'E1', "I can talk", 'Ruby')
+  student.set_student_name('Melinda')
+  assert_equal('Melinda', student.student_name)
+
+end
+
+def test_set_cohort
+  student = CodeClanStudent.new('Gillian', 'E1', "I can talk", 'Ruby')
+  student.set_cohort('E21')
+  assert_equal('E21', student.cohort)
+
+end
+
+def test_student_talk
+  student = CodeClanStudent.new('Gillian', 'E1', "I can talk", 'Ruby')
+  assert_equal("I can talk", student.talk)
+
+end
+
+def test_fav_language
+  student = CodeClanStudent.new('Gillian', 'E1', "I can talk", 'Ruby')
+  assert_equal("I love Ruby", student.fav_language)
 end
 
 end
