59c6ea1b41a91c060db44b8ffe9e51fbe00f6aaa
Alan Hunter <localpunter@sky.com>
2018-08-06 16:02:07 +0100

    part b ext

---
+++ Homework/specs/sports_team_spec.rb
@@ -0,0 +1,29 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../sports_team.rb')
+
+
+class SportsTeamTest < MiniTest::Test
+
+  def test_team_name
+    sports_team = SportsTeam.new('Celtic', ["Brown", "Tierney", "Forrest"], "Brendan")
+
+
+    assert_equal("Celtic", sports_team.team_name)
+    assert_equal(["Brown", "Tierney", "Forrest"], sports_team.players)
+    assert_equal("Brendan", sports_team.coach)
+
+  end
+
+  def test_set_coach_name
+    sports_team = SportsTeam.new('Celtic', ["Brown", "Tierney", "Forrest"], "Brendan")
+    sports_team.set_coach_name("Martin")
+
+    assert_equal("Martin", sports_team.coach)
+    
+
+
+  end
+
+
+end
